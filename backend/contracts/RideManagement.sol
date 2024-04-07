55f580e62e2334b47f5544dbe4854be0c50d8f76
Szymon <Szymonlyzwinski@yahoo.co.uk>
2024-04-07 19:53:03 +0200

    finished ride contract and added price feed

--- backend/contracts/RideManagement.sol
+++ backend/contracts/RideManagement.sol
@@ -1,9 +1,12 @@
-pragma solidity 0.8.24;
+pragma solidity 0.8.20;
 
 import "contracts/UserManagement.sol";
+import "contracts/PriceFeed.sol";
 
 contract RideManagement {
 UserManagement public userContract;
+AggregatorV3Interface private priceFeed;
+using PriceConverter for uint256;
 
 struct Ride {
     uint id;
@@ -23,13 +26,18 @@ mapping(address => Ride) usersRides;
 mapping(uint => Ride) rides;
 uint256 currentrideID = 0;
 
+constructor(address userManagementAddy, address pricefeedAddy){
+        userContract = UserManagement(userManagementAddy);
+        priceFeed = AggregatorV3Interface(pricefeedAddy);
+}
+
 function createRide(string memory _pickup, string memory _destination, string memory _date, 
                     uint _time, uint _seats, uint _price) external 
     {
         require(userContract.getUser(address(msg.sender)).hasCar, "user needs car to post ride");
         currentrideID++;
 
-        Ride storage  newRide = rides[currentrideID](
+        Ride memory newRide = Ride(
             currentrideID, 
             _pickup,
             _destination,
@@ -37,14 +45,34 @@ function createRide(string memory _pickup, string memory _destination, string me
             _time,
             _seats,
             _price,
-            _time,
-            _seats,
-            _price
+            new address[](_seats)
         );
 
-
-
+        usersRides[msg.sender] = newRide;
+        rides[currentrideID] = newRide;
+        emit ridePosted(msg.sender, _pickup, _destination, _time);
     }
 
+    function BuyRide(uint chosenRide) external payable {
+        require(chosenRide <= currentrideID, "chosen invalid ride");
 
+        Ride storage currentRide = rides[chosenRide];
+        require(msg.value >= currentRide.price.getConversionRate(priceFeed), "pls send enough eth to cover ride");
+        require(currentRide.numOfSeatsAvaiable > 0, "ride sold out");
+
+        for(uint index = 0; index < currentRide.passengers.length; index++){
+            if(currentRide.passengers[index] == address(0)){
+                currentRide.passengers[index] = address(msg.sender);
+                break;
+            }
+        }
+
+        currentRide.numOfSeatsAvaiable -= 1;
+        emit seatBought(chosenRide, msg.sender, currentRide.pickup, currentRide.destination, currentRide.pickupTime);
+    }
+
+        function convertTest(uint amount) public view returns (uint256) {
+        uint256 converted = amount.getConversionRate(priceFeed);
+        return converted;
+    }
 }
